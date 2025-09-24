41371efe70f0c2ad15ff8899ca1913a48cd94169
nasmin-azz <nastaran.atlas@gmail.com>
2025-09-24 15:31:51 +0330

    optional functions

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // function hello (name: string): string {
 //   return `Hello, ${name}!`
 // }
-function logMessage(message) {
-    console.log(`hello message:${message}`);
-}
-logMessage('Hello, World!');
-logMessage('Hello, Nassim!');
-export {};
+// function logMessage(message: string): void {
+//   console.log(`hello message:${message}`);
+// }
+// logMessage('Hello, World!');
+// logMessage('Hello, Nassim!');
 //  console.log(hello('Nassim'));    
+function personInfo(name, age) {
+    return age ? `${name} is ${age} years old.` : `${name}'s age is unknown.`;
+}
+console.log(personInfo('Nassim', 30)); // Nassim is 30 years old.
+console.log(personInfo('Alice')); // Alice's age is unknown.
+console.log(personInfo('Bob', undefined)); // Bob's age is unknown.
+console.log(personInfo('Charlie', 25)); // Charlie is 25 years old.
+export {};
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,11 +2,19 @@
 //   return `Hello, ${name}!`
 // }
 
-function logMessage(message: string): void {
-  console.log(`hello message:${message}`);
-}
-logMessage('Hello, World!');
-logMessage('Hello, Nassim!');
+// function logMessage(message: string): void {
+//   console.log(`hello message:${message}`);
+// }
+// logMessage('Hello, World!');
+// logMessage('Hello, Nassim!');
 
 
 //  console.log(hello('Nassim'));    
+
+function personInfo(name: string, age?: number): string {
+    return age ? `${name} is ${age} years old.` : `${name}'s age is unknown.`;
+}
+console.log(personInfo('Nassim', 30)); // Nassim is 30 years old.
+console.log(personInfo('Alice'));    // Alice's age is unknown.
+console.log(personInfo('Bob', undefined)); // Bob's age is unknown.
+console.log(personInfo('Charlie', 25)); // Charlie is 25 years old.
